98ad66029603420ed634f1b320d2f2539fd501b3
zawar7234 <fast7320398@gmail.com>
2025-10-18 21:35:13 +0500

    Create config.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBH_0gqHT0iFsOMWU6kmV0AULxk-6n0gBs",
+  authDomain: "get-296e5.firebaseapp.com",
+  projectId: "get-296e5",
+  storageBucket: "get-296e5.appspot.com",
+  messagingSenderId: "933048317983",
+  appId: "1:933048317983:web:996fd36a04b31e99813d2a",
+  measurementId: "G-8JQKV59NEM"
+};
+￼Enter
